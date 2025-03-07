2f69c0fc914e98b01f729602df177e043cb15a6d
Gabriel Longarai <gabrielslongarai@gmail.com>
2025-03-07 16:52:25 -0300

    Program class

--- GeometricShapes/Program.cs
+++ GeometricShapes/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+
+namespace Program
+{
+    class Program
+    {
+        static void Main()
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Triangle x, y;
+            x = new Triangle();
+            y = new Triangle();
+
+            Triangle.SetTriangle(x);
+            Triangle.SetTriangle(y);
+
+            Triangle.PrintTriangleArea(x);
+            Triangle.PrintTriangleArea(y);
+
+            Triangle.ComparingTriangleArea(x, y);
+            Triangle.PrintLargestArea(x, y);
+        }
+    }
+
+}
