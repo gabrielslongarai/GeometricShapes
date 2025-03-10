d13aa1fe52a99db39ef45ab506030961092f8686
Gabriel Longarai <gabrielslongarai@gmail.com>
2025-03-10 13:34:40 -0300

    Adding a Menu

--- GeometricShapes/Program.cs
+++ GeometricShapes/Program.cs
@@ -7,18 +7,43 @@ namespace GeometricShapes
 {
     class Program
     {
+
+        static void Menu()
+        {
+            int opcao;
+            Console.WriteLine("1 - Triangle");
+            Console.WriteLine("2 - Rectangle");
+            Console.WriteLine("3 - Circle");
+            Console.WriteLine("4 - Exit");
+
+            Console.Write("\nChoose an option: ");
+
+            opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Triangle a = new Triangle();
+                    Console.WriteLine(a);
+                    break;
+                case 2:
+                    Rectangle b = new Rectangle();
+                    Console.WriteLine(b);
+                    break;
+                case 3:
+                    Circle c = new Circle();
+                    Console.WriteLine(c);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+        }
         static void Main()
         {
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
-            Triangle a = new Triangle();
-            Console.WriteLine(a);
-
-            Rectangle b = new Rectangle();
-            Console.WriteLine(b);
-
-            Circle c = new Circle();
-            Console.WriteLine(c);
+            Menu();
         }
     }
 
